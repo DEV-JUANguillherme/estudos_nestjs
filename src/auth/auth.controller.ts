a3d1c6e4ed2e85c6c6d111fc79114f7c31dd0e74
Juan Guillherme <guilhermejuan729@gmail.com>
2024-12-04 22:07:47 -0300

    feat(controller): implements controller post signUp

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+    constructor(private readonly authService: AuthService){}
+
+    @Post('signUp')
+    // vamos então criar nosso método de sign up
+    signUp(@Body() body: any){
+        // aqui vamos criar um novo usuário
+        const {email, password} = body;
+        return this.authService.singUp(email, password);
+    }
+}
